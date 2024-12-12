2a99514f6d549e9751833f2006a9f4525b2ba713
Ayilara Aisha <ayilaraaisha10@gmail.com>
2024-12-13 00:47:56 +0100

    Tracker

--- TaskTracker/obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
+++ TaskTracker/obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c594627f8b06d70fd45c4eff2f461b893ff41aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
